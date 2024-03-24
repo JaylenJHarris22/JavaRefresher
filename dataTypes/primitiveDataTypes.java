2226b9406aafca57abb637998407b0cffc034ce5
Jaylen Harris <harris.j.jaylen22@gmail.com>
2024-03-23 21:08:20 -0400

    used all of the primitive types in appropriate use cases

--- dataTypes/primitiveDataTypes.java
+++ dataTypes/primitiveDataTypes.java
@@ -1,7 +1,28 @@
 package dataTypes;
 public class primitiveDataTypes {
     public static void main(String[] args) {
+        byte data = 127;
+        System.out.println(data);
+
+        short smallNum = 32434;
+        System.out.println(smallNum);
+
         int num = 21;
         System.out.println(num);
+
+        long bigNum = 234454343L;
+        System.out.println(bigNum);
+
+        float decimalNum = 34.5F;
+        System.out.println(decimalNum);
+
+        double bigDecimalNum = 24242424.4244;
+        System.out.println(bigDecimalNum);
+
+        char letter = 'J';
+        System.out.println(letter);
+
+        boolean bool = true;
+        System.out.println(bool);
     }
 }
