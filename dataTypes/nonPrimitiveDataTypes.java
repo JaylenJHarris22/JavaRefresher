816d7a0ed782a5e939b8ce153601c58feff45dff
Jaylen Harris <harris.j.jaylen22@gmail.com>
2024-03-26 18:20:30 -0400

    added the array to the non primitive data types

--- dataTypes/nonPrimitiveDataTypes.java
+++ dataTypes/nonPrimitiveDataTypes.java
@@ -8,5 +8,12 @@ public class nonPrimitiveDataTypes {
         //object
         int num = 21;
         Student Jaylen = new Student(word, num);
+        System.out.println(Jaylen.name + " " + Jaylen.age);
+
+        //array
+        String[] family = {"Jaylen", "Quinn", "Aaliyah"};
+        for (String name : family){
+            System.out.println(name);
+        }
     }
 }
