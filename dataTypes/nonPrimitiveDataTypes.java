a08dc5295a3a95c1241d21225a71e9f8341990b4
Jaylen Harris <harris.j.jaylen22@gmail.com>
2024-03-25 20:38:32 -0400

    non primitive data types and created student class

---
+++ dataTypes/nonPrimitiveDataTypes.java
@@ -0,0 +1,12 @@
+package dataTypes;
+
+public class nonPrimitiveDataTypes {
+    public static void main(String[]args){
+        String word = "Jaylen";
+        System.out.println(word);
+
+        //object
+        int num = 21;
+        Student Jaylen = new Student(word, num);
+    }
+}
